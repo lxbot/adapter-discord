5a9fd8dcd1d2e71ca2af2f2d1057a99a1c5533a5
mohemohe <mohemohe@ghippos.net>
2024-07-16 02:24:10 +0900

    v2 ready

--- adapter.go
+++ adapter.go
@@ -2,38 +2,27 @@ package main
 
 import (
 	"bytes"
-	"encoding/gob"
-	"github.com/bwmarrin/discordgo"
-	"github.com/lxbot/lxlib"
 	"log"
 	"os"
 	"strings"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/lxbot/lxlib/v2"
+	"github.com/lxbot/lxlib/v2/common"
+	"github.com/lxbot/lxlib/v2/lxtypes"
 )
 
-type M = map[string]interface{}
+var adapter *lxlib.Adapter
+var messageCh *chan *lxtypes.Message
 
-var ch *chan M
 var client *discordgo.Session
 var token string
 
-func Boot(c *chan M) {
-	ch = c
+func init() {
+	adapter, messageCh = lxlib.NewAdapter()
+}
 
-	gob.Register(discordgo.MessageCreate{})
-	gob.Register(discordgo.Message{})
-	gob.Register(discordgo.MessageAttachment{})
-	gob.Register(discordgo.MessageEmbed{})
-	gob.Register(discordgo.MessageEmbedAuthor{})
-	gob.Register(discordgo.MessageEmbedField{})
-	gob.Register(discordgo.MessageEmbedFooter{})
-	gob.Register(discordgo.MessageEmbedImage{})
-	gob.Register(discordgo.MessageEmbedProvider{})
-	gob.Register(discordgo.MessageEmbedThumbnail{})
-	gob.Register(discordgo.MessageEmbedVideo{})
-	gob.Register(discordgo.MessageReaction{})
-	gob.Register(discordgo.Emoji{})
-	gob.Register(discordgo.Member{})
-
+func main() {
 	token = os.Getenv("LXBOT_DISCORD_BOT_TOKEN")
 	if token == "" {
 		log.Fatalln("invalid token:", "'LXBOT_DISCORD_BOT_TOKEN' にアクセストークンを設定してください")
@@ -41,99 +30,96 @@ func Boot(c *chan M) {
 	var err error
 	client, err = discordgo.New("Bot " + token)
 	if err != nil {
-		log.Fatalln(err)
+		common.FatalLog(err)
 	}
-	client.AddHandler(onMessage)
+	client.AddHandler(onDiscordMessage)
+
+	// REF: https://github.com/bwmarrin/discordgo/issues/1270
+	client.Identify.Intents |= discordgo.IntentMessageContent
 
 	err = client.Open()
 	if err != nil {
-		log.Println("error opening connection:", err)
+		common.FatalLog("error opening connection:", err)
 	}
+	listenLxbotMessage()
 }
 
-func Send(msg M) {
-	m, err := lxlib.NewLXMessage(msg)
-	if err != nil {
-		log.Println(err)
-		return
-	}
+func listenLxbotMessage() {
+	for {
+		message := <-*messageCh
 
-	texts := split(m.Message.Text, 2000)
-	for _, v := range texts {
-		_, err := client.ChannelMessageSend(m.Room.ID, v)
-		if err != nil {
-			log.Println(err)
-			return
+		mention := ""
+		if message.Mode == lxtypes.ReplyMode {
+			mention = "<@" + message.User.ID + "> "
+		}
+
+		for _, v := range message.Contents {
+			texts := split(v.Text, 2000-len(mention))
+			for _, v := range texts {
+				_, err := client.ChannelMessageSend(message.Room.ID, mention+v)
+				if err != nil {
+					common.DebugLog(err)
+					return
+				}
+			}
 		}
 	}
 }
 
-func Reply(msg M) {
-	m, err := lxlib.NewLXMessage(msg)
-	if err != nil {
-		log.Println(err)
+func onDiscordMessage(sess *discordgo.Session, discord *discordgo.MessageCreate) {
+	if discord.Author.ID == sess.State.User.ID {
+		common.TraceLog("onDiscordMessage()", "ignore myself")
 		return
 	}
 
-	mention := "<@" + m.User.ID + "> "
+	text := strings.TrimSpace(discord.Content)
+	if text == "" {
+		common.TraceLog("onDiscordMessage()", "ignore empty message")
+		return
+	}
 
-	texts := split(m.Message.Text, 2000 - len(mention))
-	for _, v := range texts {
-		_, err := client.ChannelMessageSend(m.Room.ID, mention + v)
-		if err != nil {
-			log.Println(err)
-			return
+	message := &lxtypes.Message{}
+
+	message.User = lxtypes.User{
+		ID:   discord.Author.ID,
+		Name: discord.Author.Username,
+	}
+
+	message.Room = lxtypes.Room{
+		ID:          discord.ChannelID,
+		Name:        discord.ChannelID,
+		Description: "",
+	}
+	if st, err := client.Channel(discord.ChannelID); err == nil {
+		message.Room.Name = st.Name
+	}
+
+	attachments := make([]lxtypes.Attachment, len(discord.Attachments))
+	for i, v := range discord.Attachments {
+		attachments[i] = lxtypes.Attachment{
+			Url:         v.URL,
+			Description: v.Filename,
 		}
 	}
-}
 
-func onMessage(sess *discordgo.Session, msg *discordgo.MessageCreate) {
-	if msg.Author.ID == sess.State.User.ID {
-		return
-	}
+	message.Contents = make([]lxtypes.Content, 0)
+	message.Contents = append(message.Contents, lxtypes.Content{
+		ID:          discord.ID,
+		Text:        text,
+		Attachments: attachments,
+	})
 
-	text := strings.TrimSpace(msg.Content)
-
-	isReply := false
-	for _, v := range msg.Mentions {
+	for _, v := range discord.Mentions {
 		if v.ID == sess.State.User.ID {
-			isReply = true
+			message.Mode = lxtypes.ReplyMode
 		}
 	}
 
-	attachments := make([]M, len(msg.Attachments))
-	for i, v := range msg.Attachments {
-		attachments[i] = M{
-			"url": v.URL,
-			"description": v.Filename,
-		}
+	if raw, err := common.ToJSON(discord); err == nil {
+		message.Raw = raw
 	}
 
-	channelName := msg.ChannelID
-	topic := ""
-	if i, err := client.Channel(msg.ChannelID); err == nil {
-		channelName = i.Name
-		topic = i.Topic
-	}
-
-	*ch <- M{
-		"user": M{
-			"id":   msg.Author.ID,
-			"name": msg.Author.Username,
-		},
-		"room": M{
-			"id":          msg.ChannelID,
-			"name":        channelName,
-			"description": topic,
-		},
-		"message": M{
-			"id":          msg.ID,
-			"text":        text,
-			"attachments": attachments,
-		},
-		"is_reply":  isReply,
-		"raw":       *msg,
-	}
+	adapter.Send(message)
 }
 
 func split(s string, n int) []string {
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/lxbot/adapter-discord
 go 1.13
 
 require (
-	github.com/bwmarrin/discordgo v0.20.2
+	github.com/bwmarrin/discordgo v0.28.1
 	github.com/lxbot/lxlib v1.4.1
+	github.com/lxbot/lxlib/v2 v2.0.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 )
